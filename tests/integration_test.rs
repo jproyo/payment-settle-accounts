907c5997953da92e4c5287c319284006ad13127f
Juan Pablo Royo Sales <juanpablo.royo@gmail.com>
2023-09-29 17:35:19 +0200

    Adding doc

--- tests/integration_test.rs
+++ tests/integration_test.rs
@@ -21,3 +21,25 @@ fn test_process_with_correct_results() {
 
     assert_eq!(obtained, &expected);
 }
+
+#[test]
+fn test_process_with_correct_results_with_chargebacks() {
+    let mut csv_reader = CSVTransactionReader::new("tests/data/tx_test_with_charge_back.csv");
+    let mut engine = MemoryThreadSafePaymentEngine::new();
+    for record in csv_reader.iter() {
+        engine.process(&record.unwrap()).unwrap();
+    }
+    let result = engine.summary();
+    assert_eq!(result.len(), 2);
+
+    let expected = TransactionResult::builder()
+        .client_id(1_u16)
+        .available(0.5)
+        .held(0.0)
+        .locked(true)
+        .build();
+
+    let obtained = result.iter().find(|x| x.client_id() == 1_u16).unwrap();
+
+    assert_eq!(obtained, &expected);
+}
